6c6c1c28e120e21ca469c3e411f2f56475af6753
Ward Vandewege <ward@curoverse.com>
2016-05-16 21:00:08 -0400

    Remove a bunch of files that were moved to the arvados repository.
    No issue #

--- jenkins/package-testing/test-package-python27-python-arvados-fuse.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-exec python <<EOF
-import arvados_fuse
-print "Successfully imported arvados_fuse"
-EOF
--- jenkins/package-testing/test-package-python27-python-arvados-python-client.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-exec python <<EOF
-import arvados
-print "Successfully imported arvados"
-EOF
