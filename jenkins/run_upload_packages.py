3fb4f8f36961b7eebf667336bb1fadbcf7a1cf4a
Ward Vandewege <ward@curoverse.com>
2017-01-28 11:56:05 -0500

    Remove debian7 from our package upload script.
    refs #10785

--- jenkins/run_upload_packages.py
+++ jenkins/run_upload_packages.py
@@ -181,7 +181,6 @@ freight cache "apt/$DISTNAME"
 rm "$@"
 """
     TARGET_DISTNAMES = {
-        'debian7': 'wheezy',
         'debian8': 'jessie',
         'ubuntu1204': 'precise',
         'ubuntu1404': 'trusty',
@@ -232,7 +231,7 @@ PACKAGE_SUITES = {
                           'services/login-sync/*.gem',
                       ),
     }
-for target in ['debian7', 'debian8', 'ubuntu1204', 'ubuntu1404', 'ubuntu1604']:
+for target in ['debian8', 'ubuntu1204', 'ubuntu1404', 'ubuntu1604']:
     PACKAGE_SUITES[target] = _define_suite(
         DebianPackageSuite, os.path.join('packages', target, '*.deb'),
         target=target)
