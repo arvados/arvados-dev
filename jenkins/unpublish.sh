2cd13834ca9886d73e24ffb1aa66e72b30cfba76
Peter Amstutz <peter.amstutz@curii.com>
2020-02-10 14:43:10 -0500

    Fix find pattern for debs
    refs #16015
    
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- jenkins/unpublish.sh
+++ jenkins/unpublish.sh
@@ -2,17 +2,17 @@
 
 set -x
 
-VERNO=$1
-
-if [[ -z "$VERNO" ]] ;
+if [[ -z "$1" ]] ; then
    echo "Must provide version number to unpublish"
    exit 1
 fi
+
+VERNO=$1
 
 rm -f $(find /var/www/rpm.arvados.org/CentOS/7/testing -name *-${VERNO}*.rpm)
 createrepo /var/www/rpm.arvados.org/CentOS/7/testing/x86_64
 
 for fr in $(ls -d /var/lib/freight/apt/*-testing) ; do
-    rm -f $(find ${fr} -name *-${VERNO}*.deb)
+    rm -f $(find ${fr} -name *_${VERNO}*.deb)
     freight cache ${fr}
 done
