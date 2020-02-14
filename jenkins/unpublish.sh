7c440b026d4be0e66d8da6a1529928ebef86ca44
Peter Amstutz <tetron@public.arvados.org>
2020-02-14 15:56:26 +0000

    Can unpublish an individual package

--- jenkins/unpublish.sh
+++ jenkins/unpublish.sh
@@ -3,16 +3,22 @@
 set -x
 
 if [[ -z "$1" ]] ; then
-   echo "Must provide version number to unpublish"
-   exit 1
+    echo "Must provide version number to unpublish"
+    exit 1
 fi
 
 VERNO=$1
+PACKAGE="*"
 
-rm -f $(find /var/www/rpm.arvados.org/CentOS/7/testing -name *-${VERNO}*.rpm)
-createrepo /var/www/rpm.arvados.org/CentOS/7/testing/x86_64
+if [[ -n "$2" ]] ; then
+    PACKAGE="$2"
+fi
+
+rm -f $(find /var/www/rpm.arvados.org/CentOS/7/testing -name "${PACKAGE}-${VERNO}*.rpm")
+sudo -u jenkinsapt createrepo /var/www/rpm.arvados.org/CentOS/7/testing/x86_64
 
 for fr in $(ls -d /var/lib/freight/apt/*-testing) ; do
-    rm -f $(find ${fr} -name *_${VERNO}*.deb)
-    freight cache ${fr}
+    rm -f $(find ${fr} -name "${PACKAGE}_${VERNO}*.deb")
+    sudo -u jenkinsapt freight cache ${fr}
 done
+
