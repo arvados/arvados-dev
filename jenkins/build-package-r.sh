e877db7a004dda59251cbfc6563a092b4531b230
Peter Amstutz <peter.amstutz@curii.com>
2020-09-25 10:22:17 -0400

    Update build-package-r.sh to use r.arvados.org refs #16885
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- jenkins/build-package-r.sh
+++ jenkins/build-package-r.sh
@@ -7,5 +7,5 @@
 sed -i -e s/"^Version: .*$"/"Version: $VERNO"/g $WORKSPACE/sdk/R/DESCRIPTION
 cd $WORKSPACE/sdk/
 R CMD build R
-scp -p2222 $WORKSPACE/sdk/ArvadosR_"$VERNO".tar.gz jenkinsapt@public.curoverse.com:/var/www/r.arvados.org/src/contrib
-ssh -p2222 jenkinsapt@public.curoverse.com 'cd /var/www/r.arvados.org/src/contrib && R -q -e "tools::write_PACKAGES()"'
+scp -p2222 $WORKSPACE/sdk/ArvadosR_"$VERNO".tar.gz jenkinsapt@r.arvados.org:/var/www/r.arvados.org/src/contrib
+ssh -p2222 jenkinsapt@r.arvados.org 'cd /var/www/r.arvados.org/src/contrib && R -q -e "tools::write_PACKAGES()"'
