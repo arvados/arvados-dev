29cd7deb30b7900b08f6ef92d335b857d86e85e7
Ward Vandewege <ward@curii.com>
2021-05-05 11:16:16 -0400

    Bump go to 1.16.3.
    No issue #
    
    Arvados-DCO-1.1-Signed-off-by: Ward Vandewege <ward@curii.com>

--- jenkins/packer-images/jenkins-image-arvados-tests.sh
+++ jenkins/packer-images/jenkins-image-arvados-tests.sh
@@ -7,14 +7,23 @@
 set -eo pipefail
 
 # Install the dependencies for arvados-server
-sudo su -c "DEBIAN_FRONTEND=noninteractive apt-get install -y libpam0g-dev golang-1.14"
+sudo su -c "DEBIAN_FRONTEND=noninteractive apt-get install -y libpam0g-dev wget"
+
+# Get Go 1.16.3
+cd /usr/src
+sudo wget https://golang.org/dl/go1.16.3.linux-amd64.tar.gz
+sudo tar xzf go1.16.3.linux-amd64.tar.gz
+sudo ln -s /usr/src/go/bin/go /usr/local/bin/go-1.16.3
+sudo ln -s /usr/src/go/bin/gofmt /usr/local/bin/gofmt-1.16.3
+sudo ln -s /usr/local/bin/go-1.16.3 /usr/local/bin/go
+sudo ln -s /usr/local/bin/gofmt-1.16.3 /usr/local/bin/gofmt
 
 # Check out a local copy of the arvados repo so we can use it to install the dependencies
 cd /usr/src
 sudo git clone arvados.git
 cd arvados
-/usr/lib/go-1.14/bin/go mod download
-sudo /usr/lib/go-1.14/bin/go run ./cmd/arvados-server install -type test
+go mod download
+sudo go run ./cmd/arvados-server install -type test
 
 # Our Jenkins jobs use this directory to store the temporary files for the tests
 mkdir /home/jenkins/tmp
