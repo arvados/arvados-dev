a00611e14c01960fa453e787c2a35c47fb6edcb4
Javier Bértoli <jbertoli@curii.com>
2021-07-01 14:39:05 -0300

    17844: use curl to get files
    Arvados-DCO-1.1-Signed-off-by: Javier Bértoli <jbertoli@curii.com>

--- jenkins/packer-images/jenkins-image-arvados-formula-tests.sh
+++ jenkins/packer-images/jenkins-image-arvados-formula-tests.sh
@@ -11,8 +11,8 @@ sudo su -c "DEBIAN_FRONTEND=noninteractive apt-get install -y docker.io git ruby
 sudo usermod -a -G docker jenkins
 
 cd /tmp
-wget -c https://git.arvados.org/arvados-formula.git/blob_plain/refs/heads/main:/Gemfile
-wget -c https://git.arvados.org/arvados-formula.git/blob_plain/refs/heads/main:/Gemfile.lock
+curl -L https://git.arvados.org/arvados-formula.git/blob_plain/refs/heads/main:/Gemfile -o /tmp/Gemfile
+wget -L https://git.arvados.org/arvados-formula.git/blob_plain/refs/heads/main:/Gemfile.lock -o /tmp/Gemfile.lock
 sudo su -c "gem install bundler"
 sudo su -c "bundler install"
 sudo su -c "gem install kitchen-docker"
