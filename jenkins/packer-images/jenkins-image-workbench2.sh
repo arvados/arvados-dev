ebe111f73372695ac4c52529d4346840763e1811
Ward Vandewege <ward@curii.com>
2021-07-06 17:23:15 -0400

    Allow building a wb2 jenkins image from a specific wb2 git commit.
    refs #17782
    
    Arvados-DCO-1.1-Signed-off-by: Ward Vandewege <ward@curii.com>

--- jenkins/packer-images/jenkins-image-workbench2.sh
+++ jenkins/packer-images/jenkins-image-workbench2.sh
@@ -9,9 +9,14 @@ set -eo pipefail
 # Get the wb2 repository
 cd /usr/src
 sudo git clone https://git.arvados.org/arvados-workbench2.git
+cd arvados-workbench2
+
+if [[ "$GIT_HASH" != "" ]]; then
+  echo "GIT_HASH is set to $GIT_HASH, checking out that revision..."
+  sudo git checkout $GIT_HASH
+fi
 
 # Build the workbench2-build docker image
-cd arvados-workbench2
 sudo make workbench2-build-image
 
 cd ..
