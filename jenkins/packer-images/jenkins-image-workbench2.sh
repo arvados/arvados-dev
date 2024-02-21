6facdd63defe8f1d58c136881424c1d05f5b0c71
Lucas Di Pentima <lucas@di-pentima.com.ar>
2024-02-21 11:28:32 -0300

    Updates WB2 image building script for Jenkins. Refs #21461
    Arvados-DCO-1.1-Signed-off-by: Lucas Di Pentima <lucas.dipentima@curii.com>

--- jenkins/packer-images/jenkins-image-workbench2.sh
+++ jenkins/packer-images/jenkins-image-workbench2.sh
@@ -6,10 +6,10 @@
 
 set -eo pipefail
 
-# Get the wb2 repository
+# Get the arvados repository
 cd /usr/src
-sudo git clone https://git.arvados.org/arvados-workbench2.git
-cd arvados-workbench2
+sudo git clone https://git.arvados.org/arvados.git
+cd arvados/services/workbench2
 
 if [[ "$GIT_HASH" != "" ]]; then
   echo "GIT_HASH is set to $GIT_HASH, checking out that revision..."
@@ -23,5 +23,5 @@ echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf
 # Build the workbench2-build docker image
 sudo make workbench2-build-image
 
-cd ..
-sudo rm -rf arvados-workbench2
+cd ../../../
+sudo rm -rf arvados
