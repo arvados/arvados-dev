7c6a08bbe198605f76b44b708380b27882c1b237
Brett Smith <brett.smith@curii.com>
2024-09-19 16:31:00 -0400

    21909: Update build-package-r to build the package with make
    This is necessary to generate code and documentation before packaging
    everything up. The rest of the changes are just path adjustments now
    that the script works out of `sdk/R/` instead of just `sdk/`.
    Refs #21909.
    
    Arvados-DCO-1.1-Signed-off-by: Brett Smith <brett.smith@curii.com>

--- jenkins/build-package-r.sh
+++ jenkins/build-package-r.sh
@@ -4,8 +4,12 @@
 # SPDX-License-Identifier: AGPL-3.0
 # The script receives version as a parameter and builds the package producing a tar
 # and this tarball is uploaded into the public server
-sed -i -e s/"^Version: .*$"/"Version: $VERNO"/g $WORKSPACE/sdk/R/DESCRIPTION
-cd $WORKSPACE/sdk/
-R CMD build R
-scp $WORKSPACE/sdk/ArvadosR_"$VERNO".tar.gz jenkinsapt@r.arvados.org:/var/www/r.arvados.org/src/contrib
+cd "$WORKSPACE/sdk/R"
+sed -i -e s/"^Version: .*$"/"Version: $VERNO"/g DESCRIPTION
+if [ -e Makefile ]; then
+    make package
+else
+    R CMD build .  # Fallback for pre-3.0 releases
+fi
+scp "ArvadosR_$VERNO.tar.gz" jenkinsapt@r.arvados.org:/var/www/r.arvados.org/src/contrib
 ssh jenkinsapt@r.arvados.org 'cd /var/www/r.arvados.org/src/contrib && R -q -e "tools::write_PACKAGES()"'
