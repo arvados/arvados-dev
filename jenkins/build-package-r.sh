80e9fb58caa18d8cc3e280cca6492cdd8f4d0040
Peter Amstutz <peter.amstutz@curii.com>
2024-11-13 18:32:13 -0500

    Script to publish R package fails fast and accepts host key
    refs #22084
    
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- jenkins/build-package-r.sh
+++ jenkins/build-package-r.sh
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: AGPL-3.0
 # The script receives version as a parameter and builds the package producing a tar
 # and this tarball is uploaded into the public server
+set -xe
 cd "$WORKSPACE/sdk/R"
 sed -i -e s/"^Version: .*$"/"Version: $VERNO"/g DESCRIPTION
 if [ -e Makefile ]; then
@@ -11,5 +12,5 @@ if [ -e Makefile ]; then
 else
     R CMD build .  # Fallback for pre-3.0 releases
 fi
-scp "ArvadosR_$VERNO.tar.gz" jenkinsapt@r.arvados.org:/var/www/r.arvados.org/src/contrib
-ssh jenkinsapt@r.arvados.org 'cd /var/www/r.arvados.org/src/contrib && R -q -e "tools::write_PACKAGES()"'
+scp -o "StrictHostKeyChecking no" "ArvadosR_$VERNO.tar.gz" jenkinsapt@r.arvados.org:/var/www/r.arvados.org/src/contrib
+ssh -o "StrictHostKeyChecking no" jenkinsapt@r.arvados.org 'cd /var/www/r.arvados.org/src/contrib && R -q -e "tools::write_PACKAGES()"'
