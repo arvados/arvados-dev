6488b8da6120e13ff7541ecaeecfa49a7b6f6867
Brett Smith <brett.smith@curii.com>
2024-12-07 08:18:02 -0500

    22383: Remove scaffolding from node-ready.sh
    See comments for rationale.
    
    Arvados-DCO-1.1-Signed-off-by: Brett Smith <brett.smith@curii.com>

--- jenkins/packer-images/node-ready.sh
+++ jenkins/packer-images/node-ready.sh
@@ -5,10 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # GCP nodes sometimes have no outbound working network for the first few
-# seconds/minutes.
+# seconds/minutes. This script will wait until git.arvados.org is reachable.
 #
-# This script will wait until git.arvados.org is reachable.
-set -eo pipefail
+# This script does NOT use `set -euo pipefail` because any failure means we
+# fail to start SSH and the node is an unusable waste of money. This should be
+# reasonably low risk as long as the script has a minimum of intermediate state.
 
 # Send all stdout/stderr to the log and to the terminal
 exec > >(tee -a /tmp/boot-wait.log) 2>&1
@@ -23,12 +24,9 @@ while ! /bin/nc -w1 -z git.arvados.org 22; do
 done
 echo "Connected!"
 
-# All set! Start sshd so jenkins can start the agent...
-systemctl list-units --all --full --plain --quiet --type=service "ssh*" |
-    awk '($1 ~ /^[^@]+\.service$/ && $2 == "loaded") { print $1; }' |
-    while read ssh_service; do
-        systemctl start "$ssh_service"
-    done
+# Debian calls it ssh.service; RHEL calls it sshd.service. Try both;
+# it's fine as long as the one we want starts.
+systemctl start ssh.service sshd.service
 
 echo "Completed node-ready.sh"
 # Log a timestamp
